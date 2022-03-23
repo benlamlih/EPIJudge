3caa769b556f1db1c1e27640d9d6b9dc341f9d97
Mohammed Benlamlih <bnelamlih.med@gmail.com>
2022-03-23 14:46:18 +0100

    first solution of parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,11 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = 0
+    while x > 0:
+        result = result + 1 if x & 1 == 1 else result + 0
+        x >>= 1
+    return 1 if result % 2 != 0 else 0
 
 
 if __name__ == '__main__':
