a0efd3e72516a382845fd5aee2d1825b3e74d0d6
Mohammed Benlamlih <bnelamlih.med@gmail.com>
2022-03-24 21:01:14 +0100

    first solution

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # -314  -> 413
+    # 42 -> 24
+    neg = False
+    if x < 0:
+        x = -x
+        neg = True
+    res = 0
+    while x:
+        res *= 10
+        res += x % 10
+        x //= 10
+
+    return -res if neg else res
 
 
 if __name__ == '__main__':
